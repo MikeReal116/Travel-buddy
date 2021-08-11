f740b2d12850e405f6a98e3b2942452e70bd7fdc
Michael <mickampo@yahoo.com>
2021-08-11 23:25:34 +0300

    project layout

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+
+import useStyles from './style';
+
+const Navbar = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className={classes.root}>
+        <AppBar position='fixed'>
+          <Toolbar>
+            <Typography variant='h6' noWrap className={classes.title}>
+              Travel Buddy
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder='Search...'
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <div className={classes.offset} />
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import { CssBaseline } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+
+import Navbar from '../components/Navbar/Navbar';
+import Map from '../components/Map/Map';
+import List from '../components/List/List';
+
+const Home = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Navbar />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Home;
